e9f33200ab3c912630beb365772961106f786c3d
Pascal Ojukwu <luvpascal.ojukwu@yahoo.com>
2024-03-28 23:33:28 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,1 @@
-import os
-
-base_url = os.path.dirname(os.path.abspath(__name__))
-class Config:
-    DEBUG = True
-    JWT_SECRET_KEY = 'X-ACCESS-LOGIN-FOUNDATIONS-PROJECT'
-    JWT_ACCESS_TOKEN_EXPIRES = 3600
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "7JNJHGHjhhjjFF"
-    UPLOAD_FOLDER = base_url
-    password = 'TestCompanion'
-    db = f'mysql+pymysql://TestCompanion:{password}@localhost:3306/TestCompanion'
-    SQLALCHEMY_DATABASE_URI = db
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_url, 'Database.db')
 
